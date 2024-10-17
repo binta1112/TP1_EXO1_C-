d4f2d82fdc9a348133ac8fa0ab9e1ea7b1df7d57
binta1112 <tbinta1112@gmail.com>
2024-10-17 23:33:43 +0200

    main

---
+++ Banque/Systeme_bancaire.cpp
@@ -0,0 +1,13 @@
+// Systeme_bancaire.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+#include "Client.h"
+
+int main()
+{
+    Client c1("Binta", courant);
+    c1.Afficher();
+
+}
+
